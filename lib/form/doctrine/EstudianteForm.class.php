0615dd11148aa244d4ae6d0fbfab9c07e658b41a
jetrias <jetrias@gmail.com>
2018-06-24 20:38:44 -0400

    agregados campos del admin

--- lib/form/doctrine/EstudianteForm.class.php
+++ lib/form/doctrine/EstudianteForm.class.php
@@ -83,7 +83,7 @@ class EstudianteForm extends BaseEstudianteForm {
         }
         $this->widgetSchema['estatus'] = new sfWidgetFormChoice(array(
                 'choices' => array('' => 'Seleccione',
-                    "ACTIVO" => "ACTIVO", "PASIVO" => "PASIVO", "REGULAR" => "REGULAR",'CONTINUANTE CON ARRASTRE' => 'CONTINUANTE CON ARRASTRE', 'NUEVO INGRESO' => 'NUEVO INGRESO',  'REINGRESO' => 'REINGRESO', 'REPITENTE' => 'REPITENTE')));
+                    "ACTIVO" => "ACTIVO", "PASIVO" => "PASIVO", "REGULAR" => "REGULAR",'CONTINUANTE CON ARRASTRE' => 'CONTINUANTE CON ARRASTRE', 'NUEVO INGRESO' => 'NUEVO INGRESO',  'REINGRESO' => 'REINGRESO', 'REPITENTE' => 'REPITENTE', 'DESERTOR' => 'DESERTOR')));
         $this->widgetSchema['ano_curso'] = new sfWidgetFormChoice(array(
             'choices' => array('' => 'Seleccione',
                 "1" => "1", "2" => "2", "3" => "3",  "4" => "4", "5" => "5", "6" => "6")));
